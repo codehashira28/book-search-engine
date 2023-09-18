4e1a6932a85137a7f30b21d27ffef03a26c896e9
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-18 18:28:14 -0500

    began including code for apollo server and graphql server system

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const gql = require('graphql-tag');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    books: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(thoughtId: ID!): Thought
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook(
+      userId: ID!
+      book: ID!
+    ): User
+    removeBook (userId: ID!, bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
